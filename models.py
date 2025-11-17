5bc7bbf4d1edb650f06af75f6cef48e0448a149e
Vishwa Geethanjana <vishwageethanjana@Geeths-MacBook-Pro.local>
2025-11-17 09:44:47 +0530

    user create

---
+++ models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel, EmailStr, Field
+
+class UserBase(BaseModel):
+    name: str = Field(..., max_length=100)
+    email: EmailStr
+    role: str
+    password: str
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+    role: str
+    created_at: datetime
+    updated_at: datetime
+
+class UserReplace(UserBase):
+    pass
+
+class UserPatch(BaseModel):
+    name: Optional[str] = Field(None, max_length=100)
+    email: Optional[EmailStr] = None
+    role: Optional[str] = None
+    password: Optional[str] = None
