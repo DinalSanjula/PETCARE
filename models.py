0b56428351cb769ef293e786f17885be7e88d2df
vishwageethanjana <geeth.me67@gmail.com>
2025-11-18 12:37:53 +0530

    Enum role model

--- models.py
+++ models.py
@@ -2,6 +2,12 @@ from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel, EmailStr, Field
 
+
+class UserRole(str, Enum):
+    ADMIN = "admin"
+    OWNER = "owner"
+    CLINIC = "clinic"
+
 class UserBase(BaseModel):
     name: str = Field(..., max_length=100)
     email: EmailStr
