401cf60720b9c12ca09a7d70cd6f5db1afee55f4
vishwageethanjana <geeth.me67@gmail.com>
2025-12-17 09:06:31 +0530

    update booking model

--- app/models/booking_model.py
+++ app/models/booking_model.py
@@ -20,5 +20,6 @@ class Booking(Base):
     start_time = Column(DateTime(timezone=True), nullable=False)
     end_time = Column(DateTime(timezone=True), nullable=False)
     status = Column(Enum(BookingStatus), nullable=False, default=BookingStatus.CONFIRMED)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
