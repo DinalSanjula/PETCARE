fad9e895c0c7950f99ce86831c6db27c64bb6aa2
vishwageethanjana <geeth.me67@gmail.com>
2025-12-20 07:38:43 +0530

    update booking model

---
+++ appointment/booking-models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, DateTime, Enum, ForeignKey
+from sqlalchemy.sql import func
+import enum
+from db import Base
+
+
+class BookingStatus(enum.Enum):
+    CONFIRMED = "CONFIRMED"
+    CANCELLED = "CANCELLED"
+    RESCHEDULED = "RESCHEDULED"
+
+class Booking(Base):
+    __tablename__ = "bookings"
+
+    id = Column(Integer , primary_key=True , index=True)
+    clinic_id = Column(Integer, ForeignKey("clinics.id"), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    #pet_id = Column()
+    start_time = Column(DateTime(timezone=True), nullable=False)
+    end_time = Column(DateTime(timezone=True), nullable=False)
+    status = Column(Enum(BookingStatus), nullable=False, default=BookingStatus.CONFIRMED)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
